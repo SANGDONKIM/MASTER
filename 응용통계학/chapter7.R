2a6fc6eb74f8bdd103ad24c914e42f820a1759c7
SANGDONKIM <kksd94@naver.com>
2020-05-13 11:21:24 +0900

    d

--- 응용통계학/chapter7.R
+++ 응용통계학/chapter7.R
@@ -101,11 +101,20 @@ result
 
 # confidence interval 
 
+# ex 7.4 
+n <- 20
+alpha <- 0.05
+x <- rnorm(n, mean = 0, sd = 2)
+UCL <- (n-1)*var(x)/qchisq(alpha, df = n-1)
+
+
+
 # ex 7.5 
 
 n <- 20
 alpha <- 0.05
 m <- 1000
+
 UCL <- numeric(m)
 for (i in 1:m) {
         x <- rnorm(n, mean = 0, sd = 2)
@@ -114,3 +123,156 @@ for (i in 1:m) {
 
 sum(UCL>4)
 mean(UCL>4)
+
+
+# ex 7.6
+
+n <- 20
+alpha <- 0.05
+m <- 1000
+UCL <- numeric(m)
+for (i in 1:m){
+        x <- rchisq(n, df = 2)
+        UCL[i] <- (n-1)*var(x)/qchisq(alpha, df = n-1)
+}
+sum(UCL>4)
+mean(UCL>4)
+
+
+# ex 7.7
+
+n <- 20
+alpha <- 0.05
+mu0 <- 500
+sigma <- 100
+m <- 10000
+
+p <- numeric(m)
+for (j in 1:m) {
+        x <- rnorm(n, mu0, sigma)
+        ttest <- t.test(x, alternative = 'greater', mu = mu0)
+        p[j] <- ttest$p.value
+}
+p
+p.hat <- mean(p<alpha)
+se.hat <- sqrt(p.hat*(1-p.hat)/m)
+print(c(p.hat, se.hat))
+
+
+
+# ex 7.8 
+
+n <- c(10, 20, 30, 50, 100, 500)
+cv <- qnorm(0.975, 0, sqrt(6/n)) # 지정된 확률에서의 검정통계량 값 
+cv
+
+sk <- function(x){
+        xbar <- mean(x)
+        m3 <- mean((x-xbar)^3)
+        m2 <- mean((x-xbar)^2)
+        return(m3/m2^1.5)
+}
+
+p.reject <- numeric(length(n))
+m <- 10000
+
+for (i in 1:length(n)) {
+        sktests <- numeric(m)
+        for (j in 1:m) {
+                x <- rnorm(n[i])
+                sktests[j] <- as.integer(abs(sk(x))>=cv[i])
+        }
+        p.reject[i] <- mean(sktests)
+}
+p.reject
+
+
+cv <- qnorm(0.975, 0, sqrt(6*(n-2)/((n+1)*(n+3))))
+round(cv, 4)
+
+
+# ex 7.9
+n <- 20
+m <- 1000
+mu0 <- 500
+sigma <- 100
+mu <- c(seq(450, 650, 10))
+M <- length(mu)
+power <- numeric(M)
+pvalues <- numeric(m)
+for (i in 1:M) {
+        mu1 <- mu[i]
+        for (j in 1:m) {
+                x <- rnorm(n, mean = mu1, sd = sigma)
+                ttest <- t.test(x, alternative = 'greater', mu = mu0)
+                pvalues[j] <- ttest$p.value
+        }
+        power[i] <- mean(pvalues<=0.05)
+}
+se <- sqrt(power*(1-power)/m)
+se
+
+library(tidyverse)
+theme_set(theme_bw())
+df <- data.frame(mean = mu, power = power, upper = power + 2*se, lower = power-2*se)
+df %>% 
+        ggplot(aes(x=mean, y=power))+
+        geom_line()+
+        geom_vline(xintercept = 500, lty = 2)+
+        geom_hline(yintercept = c(0, 0.05), lty = 1:2)+
+        geom_errorbar(aes(ymin = lower, ymax = upper), width = 0.2, lwd = 1.5)
+
+
+# pr 7.3 
+
+
+f <- function(m, n, mu0, sigma){
+        mu <- c(seq(450, 650, 10))
+        M <- length(mu)
+        power <- numeric(M)
+        pvalues <- numeric(m)
+        for (i in 1:M) {
+                mu1 <- mu[i]
+                for (j in 1:m) {
+                        x <- rnorm(n, mean = mu1, sd = sigma)
+                        ttest <- t.test(x, alternative = 'greater', mu = mu0)
+                        pvalues[j] <- ttest$p.value
+                }
+                power[i] <- mean(pvalues<=0.05)
+                se <- sqrt(power*(1-power)/m)
+        }
+        data.frame(mean = mu, power = power, upper = power + 2*se, lower = power-2*se)
+}
+
+dat1 <- cbind(f(m=1000, n=10, mu0=500, sigma=100), n=rep(10, 21))
+dat2 <- cbind(f(m=1000, n=20, mu0=500, sigma=100), n=rep(20, 21))
+dat3 <- cbind(f(m=1000, n=30, mu0=500, sigma=100), n=rep(30, 21))
+dat4 <- cbind(f(m=1000, n=40, mu0=500, sigma=100), n=rep(40, 21))
+dat5 <- cbind(f(m=1000, n=50, mu0=500, sigma=100), n=rep(50, 21))
+
+dat <- rbind(dat1, dat2, dat3, dat4, dat5)
+dat
+
+dat %>% 
+        ggplot(aes(x=mean, y=power, group = n, color = n))+
+        geom_line()+
+        geom_vline(xintercept = 500, lty = 2)+
+        geom_hline(yintercept = c(0, 0.05), lty = 1:2)+
+        geom_errorbar(aes(ymin = lower, ymax = upper), width = 0.2, lwd = 1.5)
+
+
+
+# pr 7.6
+
+n <- 20
+m <- 1000
+
+CL1 <- numeric(m)
+CL2 <- numeric(m)
+
+for (i in 1:m) {
+        x <- rchisq(n, df = 2)
+        CL1[i] <- mean(x)-qt(0.975, df = n-1)*sd(x)/sqrt(n)
+        CL2[i] <- mean(x)-qt(0.025, df = n-1)*sd(x)/sqrt(n)
+}
+mean(CL1<2&CL2>2)
