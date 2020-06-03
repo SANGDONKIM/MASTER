30e90d39035e802b026421fd028c205cda138697
SANGDONKIM <kksd94@naver.com>
2020-06-03 16:02:35 +0900

    11장

---
+++ 응용통계학/11장.R
@@ -0,0 +1,448 @@
+# Metropolis-Hastings sampler 
+
+f <- function(x, sigma){
+        if (any(x<0)) return(0)
+        stopifnot(sigma>0)
+        return((x/sigma^2)*exp(-x^2/(2*sigma^2)))
+} # 랄리 분포
+
+
+m <- 10000
+sigma <- 4
+x <- numeric(m)
+x[1] <- rchisq(1, df = 1) # 초기값
+k <- 0
+u <- runif(m)
+
+for (i in 2:m) {
+        xt <- x[i-1]
+        y <- rchisq(1, df = xt)
+        num <- f(y, sigma)*dchisq(xt, df = y)
+        den <- f(xt, sigma)*dchisq(y, df = xt)
+        if (u[i] <= num/den) 
+                x[i] <- y else {
+                x[i] <- xt
+                k <- k+1
+                }
+        }
+print(k) # reject 갯수 
+
+
+# ex 11.2
+
+b <- 2001 # 앞에 2000개를 임의로 버림 
+y <- x[b:m]
+a <- ppoints(100); a # 백분위수 계산 
+QR <- sigma*sqrt(-2*log(1-a)); QR # 이론적 랄리분포 quantile
+Q <- quantile(y, a) # 경험적 랄리 분포 quantile
+
+qqplot(QR, Q, main = '', cex = 0.5, xlab = 'Rayleigh Quantiles', ylab = 'Sample Quantiles')
+abline(0,1) 
+
+hist(y, breaks = 'scott', main = '', xlab = '', freq = F)
+lines(QR, f(QR, 4))
+
+
+# pr 11.3
+
+f <- function(x, theta, eta){
+        return(1/theta*pi*(1+((x-eta)/theta)^2))
+}
+
+
+m <- 10000
+x <- numeric(m)
+x[1] <- rnorm(1) # 초기값
+k <- 0
+u <- runif(m)
+theta = 1
+eta = 0
+
+for (i in 2:m) {
+        xt <- x[i-1]
+        y <- rnorm(1)
+        num <- f(y, theta, eta)
+        den <- f(xt, theta, eta)
+        if (u[i] <= num/den) {
+                x[i] <- y
+        } else {
+                x[i] <- xt
+                k <- k+1
+        }
+}
+print(k) # reject 갯수 
+
+
+b <- 1001 # 앞에 1000개를 임의로 버림 
+y <- x[b:m]
+a <- ppoints(10); a # 십분위수 계산 
+
+QR <- qt(a, df = 1) # theoretical
+Q <- quantile(y, a) # empirical
+
+qqplot(QR, Q, main = '', cex = 0.5, xlab = 'Cauchy Quantiles', ylab = 'Sample Quantiles')
+abline(0,1) 
+
+
+
+# ex 11.3 
+
+f.mu <- function(x){
+        exp(-lbeta(432, 5)+431*log(x)-431*log(1+x)-6*log(1+x))
+}
+
+curve(f.mu(x), from = 0, to = 400, xlab = 'hours', ylab = '') # known posterior density
+
+fr <- function(x, y){
+        a <- 431*(log(y)-log(x))
+        b <- 437*(log(1+x)-log(1+y))
+        return(exp(a+b))
+}
+
+
+m <- 10000
+x <- numeric(m)
+x[1] <- rchisq(1, df = 1)
+k <- 0
+u <- runif(m)
+for (i in 2:m) {
+        xt <- x[i-1]
+        y <- rchisq(1, df = xt)
+        r <- fr(xt, y)*dchisq(xt, df = y)/dchisq(y, df = xt)
+        if (u[i]<=r) {
+                x[i] <- y
+        } else {
+                x[i] <- xt
+                k <- k+1
+        }
+}
+
+k
+
+plot(acf(x))
+
+# ex 11.4
+
+m <- 10000
+x <- numeric(m)
+a <- 4
+x[1] <- rlnorm(1) # lognormal 난수 
+k <- 0
+u <- runif(m)
+for (i in 2:m) {
+        xt <- x[i-1]
+        y <- rgamma(1, shape = a, rate = a/xt)
+        r <- fr(xt, y)*dgamma(xt, shape = a, rate = a/y)/dgamma(y, shape = a, rate = a/xt)
+        if (u[i]<=r) {
+                x[i] <- y
+        } else {
+                x[i] <- xt
+                k <- k+1
+        }
+}
+k/m # reject 비율 
+
+plot(acf(x))
+
+
+
+
+
+# ex 11.10 (Gibbs sampler: Bivariate distribution)
+
+N <- 5000               
+burn <- 1000            
+X <- matrix(0, N, 2)    
+
+rho <- -.75             
+mu1 <- 0
+mu2 <- 2
+sigma1 <- 1
+sigma2 <- .5
+s1 <- sqrt(1-rho^2)*sigma1
+s2 <- sqrt(1-rho^2)*sigma2
+
+
+X[1, ] <- c(mu1, mu2) # 초기값
+
+for (i in 2:N) {
+        x2 <- X[i-1, 2] # x2가 주어짐
+        m1 <- mu1 + rho * (x2 - mu2) * sigma1/sigma2 # x2가 주어졌을 때 x1 조건부 분포의 평균 
+        X[i, 1] <- rnorm(1, m1, s1) # 조건부 분포로 생성된 x1 업데이트 
+        x1 <- X[i, 1]
+        m2 <- mu2 + rho * (x1 - mu1) * sigma2/sigma1 # x1이 주어졌을 때 x2 조건부 분포의 평균
+        X[i, 2] <- rnorm(1, m2, s2) # 조건부 분포로 생성된 x1 업데이트
+}
+
+b <- burn + 1
+x <- X[b:N, ] # 1000개 버림
+
+colMeans(x) # 0, 2에 거의 근사 
+cov(x)
+cor(x) # rho = -0.75에 거의 근사 
+
+plot(x, main = '', cex = 0.5, xlab = bquote(X[1]), ylab = bquote(X[2]), ylim = range(x[, 2]))
+
+
+# pr 11.9
+
+
+N <- 10000               
+burn <- 2000            
+X <- matrix(0, N, 2)    
+
+rho <- 0.9             
+mu1 <- 0
+mu2 <- 0
+sigma1 <- 1
+sigma2 <- 1
+s1 <- sqrt(1-rho^2)*sigma1
+s2 <- sqrt(1-rho^2)*sigma2
+
+
+X[1, ] <- c(mu1, mu2) # 초기값
+
+for (i in 2:N) {
+        x2 <- X[i-1, 2] # x2가 주어짐
+        m1 <- mu1 + rho * (x2 - mu2) * sigma1/sigma2 # x2가 주어졌을 때 x1 조건부 분포의 평균 
+        X[i, 1] <- rnorm(1, m1, s1) # 조건부 분포로 생성된 x1 업데이트 
+        x1 <- X[i, 1]
+        m2 <- mu2 + rho * (x1 - mu1) * sigma2/sigma1 # x1이 주어졌을 때 x2 조건부 분포의 평균
+        X[i, 2] <- rnorm(1, m2, s2) # 조건부 분포로 생성된 x1 업데이트
+}
+
+b <- burn + 1
+x <- X[b:N, ] # 1000개 버림
+
+plot(x, main = '', cex = 0.5, xlab = bquote(X[1]), ylab = bquote(X[2]), ylim = range(x[, 2]))
+
+X <- data.frame(x=X[,1], y = X[,2])
+head(X)
+
+fit <- lm(y~x, X)
+par(mfrow = c(2, 2))
+plot(fit)
+
+
+# ex 11.11
+
+Gelman.Rubin <- function(psi){
+        psi <- as.matrix(psi)
+        n <- ncol(psi)
+        k <- nrow(psi)
+        
+        psi.means <- rowMeans(psi) # 각 체인의 평균
+        B <- n*var(psi.means) # 각 체인의 평균의 분산 (between)
+        psi.w <- apply(psi, 1, 'var') # 각 체인의 분산 (within)
+        W <- mean(psi.w) # 각 체인의 분산의 평균 
+        v.hat <- W*(n-1)/n+(B/n) # 
+        r.hat <- v.hat/W # G-R stat
+        return(r.hat)
+}
+
+normal.chain <- function(sigma, N, X1){ # N : length, X1 : 초기값, sigma : normal 표준편차 
+        x <- rep(0, N)
+        x[1] <- X1
+        u <- runif(N)
+        
+        for (i in 2:N) {
+                xt <- x[i-1]
+                y <- rnorm(1, xt, sigma)  
+                r1 <- dnorm(y, 0, 1)*dnorm(xt, y, sigma)
+                r2 <- dnorm(xt, 0, 1)*dnorm(y, xt, sigma)
+                r <- r1/r2
+                if (u[i]<=r) {
+                        x[i] <- y
+                } else {
+                        x[i] <- xt
+                }
+        }
+        return(x)
+}
+
+sigma <- 4
+k <- 4
+n <- 15000
+b <- 1000
+
+x0 <- c(-10, -5, 5, 10) # 극단값으로 초기값 부여  
+X <- matrix(0, nrow = k, ncol = n) # 4x15000
+for (i in 1:k) {
+        X[i, ] <- normal.chain(sigma, n, x0[i]) # 체인 4개 생성 
+}
+
+psi <- t(apply(X, 1,cumsum)) # 각 체인의 누적합
+
+for (i in 1:nrow(psi)) {
+        psi[i, ] <- psi[i, ]/(1:ncol(psi))
+}
+print(Gelman.Rubin(psi))
+
+
+par(mfrow = c(2, 2))
+for (i in 1:k) {
+        plot(psi[i, (b+1):n], type = 'l', xlab = i, ylab = bquote(psi))
+}
+par(mfrow = c(1,1))
+
+
+rhat <- rep(0, n)
+for (j in (b+1):n) {
+        rhat[j] <- Gelman.Rubin(psi[, 1:j])
+}
+plot(rhat[(b+1):n], type = 'l', xlab = '', ylab = 'R', )
+abline(h = 1.1, lty = 2)
+
+
+
+d <- matrix(c(1,3,5,7,9,
+              2,4,6,8,10,
+              3,5,7,10,11,
+              4,5,2,5,7), byrow = T, ncol = 5, nrow = 4)
+
+d
+psi <- t(apply(d, 1, cumsum))
+psi
+for (i in 1:nrow(psi)) {
+        psi[i, ] <- psi[i, ]/(1:ncol(psi))
+}
+psi
+
+# pr 11.11
+
+
+normal.chain <- function(sigma, N, X1){ # N : length, X1 : 초기값, sigma : normal 표준편차 
+        x <- rep(0, N)
+        x[1] <- X1
+        u <- runif(N)
+        
+        for (i in 2:N) {
+                xt <- x[i-1]
+                y <- rnorm(1, xt, sigma)  
+                r1 <- dnorm(y, 0, 1)*dnorm(xt, y, sigma)
+                r2 <- dnorm(xt, 0, 1)*dnorm(y, xt, sigma)
+                r <- r1/r2
+                if (u[i]<=r) {
+                        x[i] <- y
+                } else {
+                        x[i] <- xt
+                }
+        }
+        return(x)
+}
+
+sigma <- 4
+k <- 4
+n <- 15000
+b <- 1000
+
+x0 <- c(-10, -5, 5, 10) # 극단값으로 초기값 부여  
+X <- matrix(0, nrow = k, ncol = n) # 4x15000
+for (i in 1:k) {
+        X[i, ] <- normal.chain(sigma, n, x0[i]) # 체인 4개 생성 
+}
+
+psi <- t(apply(X, 1,cumsum)) # 각 체인의 누적합
+
+for (i in 1:nrow(psi)) {
+        psi[i, ] <- psi[i, ]/(1:ncol(psi))
+}
+
+Gelman.Rubin <- function(psi){
+        psi <- as.matrix(psi) # 4x15000
+        n <- ncol(psi)
+        k <- nrow(psi)
+        i <- 3
+        # psi : 4 x 15000
+        while (TRUE) {
+                psi_iter <- psi[,(1:i)]
+                psi.means <- rowMeans(psi_iter)
+                B <- n*var(psi.means)
+                psi.w <- apply(psi_iter, 1, 'var')
+                W <- mean(psi.w)
+                v.hat <- W*(n-1)/n+(B/n)  
+                r.hat <- v.hat/W
+                if (r.hat<=1.2) {
+                        break
+                }
+                i <- i+1
+        }
+        return(c(r.hat, i))
+}
+
+
+Gelman.Rubin(psi)
+
+
+# pr 11.12 
+
+f <- function(x, sigma){
+        if (any(x<0)) return(0)
+        stopifnot(sigma>0)
+        return((x/sigma^2)*exp(-x^2/(2*sigma^2)))
+}
+
+chain <- function(sigma, N, X1){ # N : length, X1 : 초기값, sigma : normal 표준편차 
+        x <- rep(0, N)
+        x[1] <- X1
+        u <- runif(N)
+        
+        for (i in 2:N) {
+                xt <- x[i-1]
+                y <- rchisq(1, df = xt)  
+                r1 <- f(y, sigma)*dchisq(xt, df = y)
+                r2 <- f(xt, sigma)*dchisq(y, df = xt)
+                r <- r1/r2
+                if (u[i]<=r) {
+                        x[i] <- y
+                } else {
+                        x[i] <- xt
+                }
+        }
+        return(x)
+}
+
+sigma <- 4
+k <- 4
+n <- 15000
+b <- 1000
+
+x0 <- c(2, 4, 6, 8) # 극단값으로 초기값 부여  
+X <- matrix(0, nrow = k, ncol = n) # 4x15000
+for (i in 1:k) {
+        X[i, ] <- chain(sigma, n, x0[i]) # 체인 4개 생성 
+}
+
+psi <- t(apply(X, 1, cumsum)) # 각 체인의 누적합
+
+for (i in 1:nrow(psi)) {
+        psi[i, ] <- psi[i, ]/(1:ncol(psi))
+}
+
+Gelman.Rubin <- function(psi){
+        psi <- as.matrix(psi) # 4x15000
+        n <- ncol(psi)
+        k <- nrow(psi)
+        i <- 3
+        # psi : 4 x 15000
+        while (TRUE) {
+                psi_iter <- psi[,(1:i)]
+                psi.means <- rowMeans(psi_iter)
+                B <- n*var(psi.means)
+                psi.w <- apply(psi_iter, 1, 'var')
+                W <- mean(psi.w)
+                v.hat <- W*(n-1)/n+(B/n)  
+                r.hat <- v.hat/W
+                if (r.hat<=1.2) {
+                        break
+                }
+                i <- i+1
+        }
+        return(c(r.hat, i))
+}
+
+
+Gelman.Rubin(psi)
+
+
+
--- 응용통계학/9장.R
+++ 응용통계학/9장.R
@@ -1,7 +1,7 @@
 library(boot)
 library(bootstrap)
 patch
-
+dim(patch)
 set.seed(1111)
 
 theta.boot <- function(patch, i){
@@ -10,17 +10,20 @@ theta.boot <- function(patch, i){
         mean(y)/mean(z)
 }
 
+
 boot.out <- boot(patch, statistic = theta.boot, R = 2000)
 
 A <- boot.array(boot.out);A # boostrap index 
-theta.b <- boot.out$t; theta.b # se 2000개 
+theta.b <- boot.out$t; theta.b # theta 값  
 n <- NROW(patch); n # 벡터의 길이 계산. nrow() : matrix, dataframe 만 적용 가능 
 jack.se <- numeric(n)
 
 for (i in 1:n) {
-        keep <- which(A[, i]==0)
+        keep <- which(A[, i]==0) # i번 째 데이터가 포함되지 않는 열 위치 
         jack.se[i] <- sd(theta.b[keep]) # 붓스트랩 샘플의 i번째 케이스를 제외한 se 계산 
 }
+
+# jack.se : n개 theta(j)에 대해 평균, 표준편차 계산 
 
 se.bar <- mean(jack.se)
 se.se <- sqrt((n-1)*mean((jack.se-se.bar)^2))
@@ -29,6 +32,7 @@ se.se <- sqrt((n-1)*mean((jack.se-se.bar)^2))
 # ex 9.2 
 
 # initialize 
+
 y <- patch$y
 z <- patch$z
 dat <- cbind(y, z)
@@ -44,8 +48,9 @@ theta_boot <- function(dat, ind){
 
 boot.obj <- boot(dat, statistic = theta_boot, R = 2000)
 theta.hat <- boot.obj$t0; theta.hat # original se
-theta.b <- boot.obj$t; theta.b # se 2000개 
-se.boot <- sd(theta.b); se.boot # se.se
+theta.b <- boot.obj$t; theta.b # 각 bootstrap sample 의 theta hat 
+se.boot <- sd(theta.b); se.boot 
+
 sample.freq <- boot.array(boot.obj);sample.freq # boot sample index 
 se.se.reps <- numeric(n)
 
@@ -54,7 +59,6 @@ for (i in 1:n) {
         se.se.reps[i] <- sd(theta.b[keep]) # boot sample 2000 개 각각에서 i번째 관측치를 0으로 만들고 계산 
 }
 
-boot.obj
 se.bar <- mean(se.se.reps); se.bar
 se.se <- sqrt((n-1)*mean((se.se.reps-se.bar)^2))
 se.se
@@ -104,6 +108,7 @@ plot(L)
 
 # ex 9.5 
 
+set.seed(1)
 x <- ironslag$chemical
 y <- ironslag$magnetic
 
@@ -111,11 +116,11 @@ m <- 2000
 n <- NROW(x) # 벡터의 길이
 
 L1 <- lm(y~x)
-b0 <- L1$coefficients[1]
-b1 <- L1$coefficients[2]
+b0 <- L1$coefficients[1] # original
+b1 <- L1$coefficients[2] # original
 
 # run bootstrap of cases 
-
+set.seed(1)
 out <- replicate(m, expr= {
         i <- sample(1:n, replace = TRUE, size = n)
         xstar <- x[i]
@@ -126,25 +131,46 @@ out <- replicate(m, expr= {
         
 })
 
-dim(out)
+bootcase <- t(out); head(bootcase)
+meancase <- colMeans(bootcase); meancase
+sdcase <- apply(bootcase, 2, 'sd'); sdcase
 
-bootcase <- t(out)
-meancase <- colMeans(bootcase)
-sdcase <- apply(bootcase, 2, 'sd')
+# for loop 이용 
+set.seed(1)
+out <- matrix(0, nrow = m, ncol = 3)
 
-meancase
-sdcase
+for (i in 1:m) {
+        j <- sample(1:n, replace = TRUE, size = n)
+        xstar <- x[j]
+        ystar <- y[j]
+        Lb <- lm(ystar ~ xstar)
+        out[i,1] <- Lb$coeff[1]
+        out[i,2] <- Lb$coeff[2]
+        out[i,3] <- summary(Lb)$sigma
+}
 
-biasint <- mean(bootcase[,1]-b0)
-biasslope <- mean(bootcase[, 2]-b1)
+bootcase <- out
+colnames(bootcase) <- c('intercept', 'slope.xstar', 's')
+dim(bootcase)
+head(bootcase)
+
+meancase <- colMeans(bootcase); meancase
+sdcase <- apply(bootcase, 2, 'sd'); sdcase
+
+
+head(bootcase) # dim (2000, 3)
+
+biasint <- mean(bootcase[,1]-b0); biasint # 2000개의 bootstrap b0 추정량 - original b0 추정량
+biasslope <- mean(bootcase[, 2]-b1); biasslope # 2000개의 bootstrap b1 추정량 - original b1 추정량
 
 rbind(estimate = c(b0, b1), bias = c(biasint, biasslope), se = sdcase[1:2], cv = c(biasint, cv=biasslope/sdcase[1:2]))
 
 
 
-# ex 9.6
+# ex 9.6 (using boot function)
+
 library(boot)
-m <- 2000
+set.seed(1)
 stats <- function(dat, i){
         x <- dat$chemical[i]
         y <- dat$magnetic[i]
@@ -156,4 +182,212 @@ stats <- function(dat, i){
 boot.out <- boot(ironslag, statistic = stats, R = 2000)
 boot.out
 boot.out$t0
+
+head(boot.out$t)
+
+sd(boot.out$t[, 2])
+
+boottbl <- broom::tidy(boot.out)
+boottbl
+boottbl$std.error[2]
+
+
+MASS::truehist(boot.out$t[, 2], main = '', xlab = 'slopes') 
+# truehist : hist 함수와 비슷함. axes = FALSE를 지정하는 것으로 축 좌표를 그리지 않도록 할 수 있다. 
+# 서로 다른 축의 좌표를 여러개 붙일 수 있는 장점이 있음. 
+# https://shlee1990.tistory.com/677 참고 
+
+abline(v=boot.out$t0[2], lwd = 2)
+
+
+boot.ci(boot.out, index = 2, type = c('norm', 'perc', 'basic', 'bca'))
+# 각 신뢰구간이 0을 포함하지 않으므로 alpha = 0.05에서 
+# chemical과 magnetic 사이에는 선형관계가 있다고 할 수 있다. 
+
+
+# pr 9.4 
+
+mam <- MASS::mammals
+head(mam)
+
+dat <- data.frame(body = log(mam$body), brain = log(mam$brain))
+head(dat)
+
+library(boot)
+
+stats <- function(dat, i){
+        x <- dat[i, 1]
+        y <- dat[i, 2]
+        Lb <- lm(y~x)
+        s <- summary(Lb)$sigma
+        c(Lb$coefficients[1], slope = Lb$coefficients[2], s=s)
+}
+set.seed(1)
+boot.out <- boot(dat, statistic = stats, R = 2000)
+boot.out
+boot.out$t0
+
+boottbl <- broom::tidy(boot.out)
+
+regstats <- function(dat, i){
+        ystar <- dat$yhat[i]+dat$r[i]
+        xstar <- dat$x[i]
+        Lnew <- lm(ystar~xstar)
+        Lnew$coefficients
+}
+
+y <- log(mammals$brain)
+x <- log(mammals$body)
+L <- lm(y~x)
+r <- rstandard(L, sd = 1)
+r <- r-mean(r)
+df <- data.frame(r = r, x = x, yhat = L$fitted.values)
+head(df)
+
+set.seed(1)
+boot.obj <- boot(data = df, statistic = regstats, R = 2000)
+
+
+broom::tidy(boot.out)
+broom::tidy(boot.obj)
+
+
+
+
+# ex 9.7 
+library(MASS)
+cor(log(mammals$body), log(mammals$brain))
+summary(mammals)
+
+y <- log(mammals$brain)
+x <- log(mammals$body)
+
+L <- lm(y~x)
+
+m.resid <- rstandard(L, sd = 1) # modified residual : ei'
+r <- m.resid - mean(m.resid) # center the modified residual : ei'-ei bar' 
+
+m <- 1000
+n <- NROW(x)
+
+set.seed(1)
+estsErr <- matrix(0, nrow = m, ncol = 3)
+for (i in 1:m) {
+        estar <- sample(r, replace = T, size = n)
+        ystar <- L$fitted.values+estar # yhat + ei'-ei bar'. yhat : 고정 
+        Lb <- lm(ystar~x)
+        
+        estsErr[i, 1] <- Lb$coeff[1]
+        estsErr[i, 2] <- Lb$coeff[2]
+        estsErr[i, 3] <-  summary(Lb)$sigma
+}
+head(estsErr)
+
+colnames(estsErr) <- c('b0.(Intercept', 'b1.x', 's')
+
+
+# ex 9.8  
+sd(ests[, 2]) # boot se(B1)
+
+s <- summary(L)$sigma
+SSx <- (n-1)*var(x)
+se.beta1 <- sqrt(s^2/SSx) # reg se(b1)
+
+s*sqrt(1/n+mean(x)^2/SSx)
+sd(ests[,1])
+ 
+
+# summary.lm method 
+
+betas <- summary(L)$coeff; betas # reg se(b0), se(b1)
+broom::tidy(summary(L))
+
+
+# ex 9.9 (model-based resampling method)
+
+regstats <- function(dat, i){
+        ystar <- dat$yhat[i]+dat$r[i]
+        xstar <- dat$x[i]
+        Lnew <- lm(ystar~xstar)
+        Lnew$coefficients
+}
+
+y <- log(mammals$brain)
+x <- log(mammals$body)
+L <- lm(y~x)
+r <- rstandard(L, sd = 1)
+r <- r-mean(r)
+df <- data.frame(r = r, x = x, yhat = L$fitted.values)
+head(df)
+
+boot.obj <- boot(data = df, statistic = regstats, R = 2000)
+broom::tidy(boot.obj)
+
+
+# ex 9.10 
+
+library(boot)
+library(bootstrap)
+theta_boot <- function(dat, ind){
+        mean(dat[ind, ]$y / mean(dat[ind, ]$z))
+}
+
+boot.out <- boot(patch, theta_boot, R = 2000)
+infl <- empinf(boot.out, type = 'jack')
+theta.hat <- boot.out$t0
+jack <- theta.hat-infl/(nrow(patch)-1)
+
+rbind(infl, jack)
+
+
+# ex 9.11
+
+jack.after.boot(boot.out, useJ = T, stinf = F)
+
+n <- NROW(dat)
+J <- numeric(n)
+
+b.freq <- boot.array(boot.obj)
+theta.b <- boot.obj$t
+
+for (i in 1:n) {
+        keep <- which(b.freq[, i] == 0)
+        J[i] <- mean(theta.b[keep])
+}
+
+# the jackknife influence values
+
+rbind((1:n), (n-1)*(mean(J)-J))
+
+
+jack.after.boot(boot.out, useJ = T, stinf = T)
+
+
+# pr 9.5 
+
+m <- 200
+
+
+f.boot <- function(dat, i){
+        x <- dat[i, 'x']
+        y <- dat[i, 'y']
+        L <- lm(y~x)
+        r <- mean((y-L$fitted.values)^2)
+        return(r)
+} # bootstap sample 하나당 return이 나오기 때문에 r[i]를 지정해주면 안됨. 
+
+dat <- matrix(rnorm(m*2), m, 2)
+colnames(dat) <- c('x', 'y')
+
+boot.out <- boot(dat, f.boot, R = 2000)
+
+boot.out
+
+
+
+
+
+
+
+
 
